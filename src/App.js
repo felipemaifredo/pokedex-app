e941bcc5b8684f3445fc283c5032601f6f6c60bc
felipemaifredo <felipemaifredo@gmail.com>
2023-05-20 22:33:30 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [pokemons, setPokemons] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const getImg = (id) => {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`;
+  }
+
+  const GetPoke = (id) => {
+    return `https://pokeapi.co/api/v2/pokemon/${id}`;
+  }
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const pokemonPromises = [];
+      for (let i = 1; i <= 649; i++) {
+        pokemonPromises.push(fetch( GetPoke(i) ).then(response => response.json()));
+      }
+      const pokemonData = await Promise.all(pokemonPromises);
+      setPokemons(pokemonData);
+    };
+
+    fetchPokemon();
+  }, []);
+
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const filteredPokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(searchValue.toLowerCase()));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='input-box'>
+        <input type="text" value={searchValue} onChange={handleSearchChange} placeholder="Search Pokemon" />
+      </div>
+      <ul className='pokedex'>
+        {filteredPokemons.map(pokemon => (
+          <li key={pokemon.id} className={`card ${pokemon.types[0].type.name}`}>
+            <img className="card-image" alt={pokemon.name} src={ getImg(pokemon.id) }/>
+            <h2 className="card-title">{`${pokemon.id}. ${pokemon.name}`}</h2>
+            <p className="card-subtitle">{pokemon.types.map(typeInfo => typeInfo.type.name).join(' | ')}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
